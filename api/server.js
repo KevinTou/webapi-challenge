cbf92287167a542f1db0e63cf205c824176c5ed6
Kevin Tou <toukevin@gmail.com>
2019-09-06 09:51:56 -0700

    Set up server

---
+++ api/server.js
@@ -0,0 +1,18 @@
+const express = require('express');
+const actionsRouter = require('./actionsRouter');
+const projectsRouter = require('./projectsRouter');
+
+const server = express();
+
+// global middleware
+server.use(express.json());
+
+// router
+server.use('/api/projects', projectsRouter);
+// server.use('/api/actions', actionsRouter);
+
+server.get('/', (req, res) => {
+  res.send('Server is up and running!');
+});
+
+module.exports = server;
